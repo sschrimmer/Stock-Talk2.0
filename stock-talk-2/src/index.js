1165084c356ab9cd1749f4da7ec90b5939df847c
Nick <chargerfan428@aol.com>
2023-09-07 21:00:31 -0700

    stripe skeleton

--- stock-talk-2/src/index.js
+++ stock-talk-2/src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { loadStripe } from '@stripe/stripe-js';
 
+const stripePromise = loadStripe('pk_test_51NnuTDCQo2qBMXyzMFBiah3MrptUU26TlomFrK8VMfEJIeWfP8YiEvq6fbhKdAwDGdQkvLGBxMe0tAE4eU2Sk1w800zaYgqjnv');
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
---
+++ stock-talk-2/src/stripe.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+
+const PaymentForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const [paymentError, setPaymentError] = useState(null);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const { paymentMethod, error } = await stripe.createPaymentMethod({
+      type: 'card',
+      card: elements.getElement(CardElement),
+    });
+
+    if (error) {
+      setPaymentError(error.message);
+    } else {
+      console.log(paymentMethod);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <CardElement />
+      <button type="submit" disabled={!stripe}>
+        Pay
+      </button>
+      {paymentError && <div>{paymentError}</div>}
+    </form>
+  );
+};
+
+export default PaymentForm;
