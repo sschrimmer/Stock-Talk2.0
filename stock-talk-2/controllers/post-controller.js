ab389e6e06a7d6bedb5330c5fe363f8c2e7b2406
Nick <chargerfan428@aol.com>
2023-09-13 19:16:12 -0700

    added create post function

--- stock-talk-2/controllers/post-controller.js
+++ stock-talk-2/controllers/post-controller.js
@@ -1,4 +1,4 @@
-const { Post } = require('../models');
+const { Users, Post } = require('../models');
 
 module.exports = {
     addReaction(req, res) {
@@ -17,5 +17,21 @@ module.exports = {
                 console.log(err);
                 res.status(500).json(err);
             });
-        }
+        },
+        createThought(req, res) {
+            Post.create(req.body)
+              .then(({ _id }) => {
+                return Users.findOneAndUpdate(
+                  { _id: req.body.userId },
+                  { $push: { thoughts: _id } },
+                  { new: true }
+                );
+              })
+              .then((thought) =>
+                !thought
+                  ? res.status(404).json({ message: "No User found with this ID!" })
+                  : res.json(thought)
+              )
+              .catch((err) => res.status(500).json(err));
+          },
     }
