94bbe0958750c0c6bdb6ee4870a90f9fdce46f64
Nick <chargerfan428@aol.com>
2023-09-14 20:18:05 -0700

    added queries and mutations

--- stock-talk-2/server/graphql/resolvers.js
+++ stock-talk-2/server/graphql/resolvers.js
@@ -6,10 +6,45 @@ const Post = require('../models/Post');
 
 const resolvers = {
   Query: {
-    //  queries here
+    getUser: async (_, { userId }) => {
+      try {
+        const user = await User.findById(userId);
+        return user;
+      } catch (error) {
+        throw new Error('Error fetching user by ID');
+      }
+    },
+
+    getAllPosts: async () => {
+      try {
+        const posts = await Post.find();
+        return posts;
+      } catch (error) {
+        throw new Error('Error fetching posts');
+      }
+    },
   },
   Mutation: {
-    // mutations here
+    createUser: async (_, { username, email, password }) => {
+      try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = new User({ username, email, password: hashedPassword });
+        await user.save();
+        return user;
+      } catch (error) {
+        throw new Error('Error creating user');
+      }
+    },
+    
+    createPost: async (_, { title, content, userId }) => {
+      try {
+        const post = new Post({ title, content, userId });
+        await post.save();
+        return post;
+      } catch (error) {
+        throw new Error('Error creating post');
+      }
+    },
   },
 };
 
